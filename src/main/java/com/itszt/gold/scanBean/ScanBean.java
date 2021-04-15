1e6b1f8cca48e1182ec6356376b6c7262dcc8316
lijinjian <914604302@qq.com>
2021-04-15 09:45:31 +0800

    注解解析

--- src/main/java/com/itszt/gold/scanBean/ScanBean.java
+++ src/main/java/com/itszt/gold/scanBean/ScanBean.java
@@ -1,10 +1,13 @@
 package com.itszt.gold.scanBean;
 
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Component
+//@ComponentScans本质是一个Set容器
+//@ComponentScans({@ComponentScan("com.itst.gold"),@ComponentScan("com.itst.gold")})
 @ComponentScan("com.itst.gold")
 @PropertySource(value = "classpath:application.properties")
 public class ScanBean {
